405f06eecc1a026847ae8c878f5079f2a2d8d944
curso.ads2 <curso.ads2@LAB06-70>
2023-05-02 22:34:38 -0300

    Desafio do 100 ao 1

---
+++ Gauss_DesafiosLeo_30-04-23/Gauss_DesafiosLeo_30-04-23/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gauss_DesafiosLeo_30_04_23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //--------------------------------------------------------------------------------------------------
+            //DESAFIO FÁCIL: Escreva um programa que exiba os números de 1 a 100 na tela em ordem decrescente.
+            //Use 1. LINQ - 2.FOR - 3.FOREACH - 4.LIST - 5. ARRAY - 6. ARRAYLIST
+            //--------------------------------------------------------------------------------------------------
+
+            /*
+            for (int i = 100; i > 0; i--) 
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.Read();
+
+            */
+
+            //FOREACH
+
+            int[] numQuery = new int[99];
+
+            foreach (int num in Enumerable.Range(1, 100).Reverse())
+            {
+                Console.WriteLine(num);
+            }
+
+            Console.Read();
+
+            //LINQ
+
+
+
+        }
+    }
+}
