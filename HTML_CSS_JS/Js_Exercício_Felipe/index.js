01419c50d499005065619268b2391d381483c7c5
Curso ADS2 <curso.ads2@academico.local>
2023-09-03 15:40:55 -0300

    add cart

--- HTML_CSS_JS/Js_Exercício_Felipe/index.js
+++ HTML_CSS_JS/Js_Exercício_Felipe/index.js
@@ -1,46 +1,89 @@
-function carrinho() {
-    const sacola = [];
 
-    function addProduto(produto) {
-        const index = findItemIndex(produto.name)
-        if (index != -1) {
-            sacola[index].amount += produto.amount
-        }
-        else {
-            sacola.push({
-                id: sacola.length + 1,
-                name: produto.name,
-                price: produto.price,
-                description: produto.description,
-                amount: produto.amount
+(function () {
+
+    function carrinho() {
+        const sacola = [];
+
+        function addProduto(produto) {
+            const index = findItemIndex(produto.name)
+            if (index != -1) {
+                sacola[index].amount += produto.amount
             }
-            )
+            else {
+                sacola.push({
+                    id: sacola.length + 1,
+                    name: produto.name,
+                    price: produto.price,
+                    description: produto.description,
+                    amount: produto.amount
+                }
+                )
+            }
+            atualizarCarrinho();
+        }
+        function calcularTotal() {
+            let total = 0;
+
+            for (const produto of sacola) {
+                total += produto.price * produto.amount;
+            }
+            return total;
+        }
+        function getSacola() {
+            return sacola;
+        }
+        function findItemIndex(nameItem) {
+            return sacola.findIndex(produto => produto.name == nameItem);
+        }
+
+        return {
+            addProduto: addProduto,
+            getSacola: getSacola,
+            calcularTotal: calcularTotal,
         }
     }
-    function calcularTotal() {
+
+    const minhaSacola = carrinho();
+    const addBtns = document.querySelectorAll(".add-btn");
+
+    addBtns.forEach(btn => {
+        btn.addEventListener("click", () => {
+            const nomeItem = btn.getAttribute("data-name");
+            const precoItem = parseFloat(btn.getAttribute("data-preco"));
+            minhaSacola.addProduto({
+                name: nomeItem,
+                price: precoItem,
+                amount: 1
+            })
+        })
+    })
+
+
+    function atualizarCarrinho() {
+        const carrinhoItensDiv = document.getElementById("carrinho-itens");
+        const totalCarrinhoDiv = document.getElementById("total-carrinho");
+
+        carrinhoItensDiv.innerHTML = ""
+        totalCarrinhoDiv.textContent = ""
+
         let total = 0;
-
-        for (const produto of sacola) {
-            total += produto.price * produto.amount;
-        }
-        return total;
-    }
-    function getSacola() {
-        return sacola;
-    }
-    function findItemIndex(nameItem) {
-        return sacola.findIndex(produto => produto.name == nameItem);
+        minhaSacola.getSacola().forEach(item => {
+            total += item.price * item.amount;
+            carrinhoItensDiv.innerHTML += `<div class="dropdown-item">${item.name} - Quantidade: ${item.amount} - 
+        R$ ${item.price.toFixed(2)}</div>`;
+        })
+        totalCarrinhoDiv.textContent = `Total: R$ ${total.toFixed(2)}`
     }
 
-    return {
-        addProduto: addProduto,
-        getSacola: getSacola,
-        calcularTotal: calcularTotal,
-    }
-}
+}) ();
 
+
+
+/* ADCIONANDO NA MÃO
 const minhaSacola = carrinho();
 carrinho();
+
+
 
 minhaSacola.addProduto({
     name: 'X-Tudão',
@@ -64,3 +107,4 @@ minhaSacola.addProduto({
 });
 
 console.log(minhaSacola.getSacola(), minhaSacola.calcularTotal());
+*/
